fe864871a21863a92f79678203d6dcfe1c16fc17
DasomJung24 <jeongdasom6@gmail.com>
2021-02-28 23:05:04 +0900

    java practice

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,38 @@
 
 public class Main {
 	
-
+	final static int N = 15;
+	
 	public static void main(String[] args) {
 		
-//		String a = "I Love You.";
-//		if(a.contains("Love"))
-//		{
-//			System.out.println("Me too");
-//		}
-//		else
-//		{
-//			System.out.println("I Hate You.");
-//		}
-//		int score = 85;
-//		
-//		if(score >= 90)
-//		{
-//			System.out.println("A+ 입니다");
-//		}
-//		else if(score >= 80)
-//		{
-//			System.out.println("B+ 입니다");
-//		}
-//		else
-//		{
-//			System.out.println("C 입니다");
-//		}
+		String a = "I Love You.";
+		if(a.contains("Love"))
+		{
+			System.out.println("Me too");
+		}
+		else
+		{
+			System.out.println("I Hate You.");
+		}
+		int score = 85;
 		
-		String a = "Man";
+		if(score >= 90)
+		{
+			System.out.println("A+ 입니다");
+		}
+		else if(score >= 80)
+		{
+			System.out.println("B+ 입니다");
+		}
+		else
+		{
+			System.out.println("C 입니다");
+		}
+		
+		String b = "Man";
 		
 		// 자바는 String을 비교할 때 equals()을 사용 
-		if(a.equals("Man"))
+		if(b.equals("Man"))
 		{
 			System.out.println("man");
 		}
@@ -40,7 +41,8 @@ public class Main {
 			System.out.println("no man");
 		}
 		
-		if(a.equalsIgnoreCase("man"))
+		// 대소문자 구별하지 않음 
+		if(b.equalsIgnoreCase("man"))
 		{
 			System.out.println("man!");
 		}
@@ -48,6 +50,44 @@ public class Main {
 		{
 			System.out.println("no man!");
 		}
+		
+		// while
+		int i = 1, sum = 0;
+		while(i <= 1000)
+		{
+			sum += i++;
+		}
+		System.out.println("1부터 1000까지의 합은 " + sum);
+		
+		// for : 초기화 부분, 조건부분, 연산부분 
+		// 삼각형 출력하기 
+		for(int j = N; j > 0; j--)
+		{
+			for(int k = j; k > 0; k--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+		
+		// 원 출력하기 
+		for(int h = -N; h <= N; h++)
+		{
+			for(int j = -N; j <= N; j++)
+			{
+				if(h * h + j * j <= N * N)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
---
+++ src/test/eclipse/Main.java
@@ -0,0 +1,32 @@
+package test.eclipse;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// 정수를 입력받아 출력 
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수를 입력하세요 : ");
+		int i = sc.nextInt();
+		System.out.println("입력된 정수는 " + i);
+		sc.close();
+		
+		// 파일에 차례로 입력된 정수에 100을 곱해 출력 
+		File file = new File("input.txt");
+		try {
+			Scanner sc2 = new Scanner(file);
+			while(sc2.hasNextInt())
+			{
+				System.out.println(sc2.nextInt() * 1000);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("파일을 찾는 도중 에러가 발생했습니다.");
+		}
+		
+	}
+
+}
